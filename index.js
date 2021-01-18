9b20cb90666e9b76cb6d150745486b9bb0c76256
Pau Sabé <pau.sabe@icloud.com>
2021-01-18 19:56:38 +0100

    Secrets file added

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const basicAuth = require('express-basic-auth')
 const app = express()
 const port = 3000
+import { webCredentials } from './secrets.js'
 
 app.use(basicAuth({
     challenge: true,
-    users: { 'asdf': 'asdf' }
+    users: { webCredentials }
 }));
 
 app.get('/', (req, res) => {
