07f81e430ffd8f24306b680f3f0aabce1b766a03
Pau Sabé <pau.sabe@icloud.com>
2021-02-12 08:58:05 +0100

    Added npm install command when publishing

--- deploy-cpl.sh
+++ deploy-cpl.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd cpl-app
 git pull
+npm install
 expo login -u $1 -p $2
 expo publish --quiet --send-to $3 --release-channel $4 --non-interactive
--- index.js
+++ index.js
@@ -37,18 +37,21 @@ app.get('/', (req, res) => {
 
 app.get('/download', function(req, res){
   try {
-
+    console.log("download");
     res.download(
       __dirname + '/cpl-app/src/Assets/db/cpl-app.db');
 
   } catch (error) {
+    console.log("download error", error);
     res.send('Something went wrong: ' + error);
   }
 });
 
 app.post('/upload', async (req, res) => {
   try {
+    console.log("upload");
     if(!req.files) {
+      console.log("upload: cap base de dades introduïda");
       res.send('Error: cap base de dades introduïda');
     } 
     else {
@@ -63,16 +66,18 @@ app.post('/upload', async (req, res) => {
       shell.exec(
         `sh deploy-cpl.sh ${expo_user} ${expo_pass} ${expo_send} ${expo_publish_channel}`, 
         async (err, stdout, stderr) => {
+          console.log("upload: Publicació realitzada correctament");
           res.send('Publicació realitzada correctament');
         });
 
     }
   } 
   catch (err) {
+    console.log("upload error", err);
     res.status(500).send(err);
   }
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`CPL web at port ${port}`);
 })
